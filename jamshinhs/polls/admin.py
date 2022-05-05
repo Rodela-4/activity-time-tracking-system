55eeb51494b32e95f8f316ba3005c0f57e022284
egwkim <35051914+egwkim@users.noreply.github.com>
2022-05-05 10:53:03 +0900

    Migrate github repo
    Migrate github repo to RODELA5
    
    Hide secret key from settings.py

---
+++ jamshinhs/polls/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Apply, Activity, User, Additonal_hour, Plan
+
+class Additonal_hourInline(admin.TabularInline):
+    model = Additonal_hour
+
+class PlanInline(admin.TabularInline):
+    model = Plan
+
+class ApplyAdmin(admin.ModelAdmin):
+    list_filter = ('activity','student_id',)
+    list_display = ('student_id', 'name', 'activity', 'state', 'completed_time',)
+    inlines = [Additonal_hourInline]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('studentid', 'name', 'password', 'completed_time',)
+
+class ActivityAdmin(admin.ModelAdmin):
+    inlines = [PlanInline]
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'title', 'due_date')
+
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Apply, ApplyAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Additonal_hour)
+admin.site.register(Plan, PlanAdmin)
